f9af1507ae3df9de58c9222f5d7e1664defb692a
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-11-05 23:15:01 -0300

    prueba

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <h1>Henry Food </h1>
+      <p>nav</p>
+      <p>ExampleCard</p>
+      
     </div>
   );
 }
