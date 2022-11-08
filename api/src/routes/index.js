f7fa03e3d1e63e4ab09da8848a03094fd7340422
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-11-08 19:13:39 -0300

    api updt

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,21 @@
 const { Router } = require('express');
+const router = Router();
+/// importo router de express
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
-const router = Router();
+// const {Diet} = require('../db')
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/recipes',(req,res)=>{
+    const name = req.query.name;
+    res.send(name)
+    
+})
+
+
 
 
 module.exports = router;
