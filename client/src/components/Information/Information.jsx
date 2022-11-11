4db0725ca6b28687e65297d574558f882188f59b
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-11-11 20:31:40 -0300

    update

--- client/src/components/Information/Information.jsx
+++ client/src/components/Information/Information.jsx
@@ -10,13 +10,27 @@ export const Create = () => {
   return (
     <div>
       <h2>Dish information</h2>
+        <img src="" alt="Dish-Information" />
         <p>Name</p>
-        <p>Name</p>
+        <p>DishTypes</p>
+        <p>DietTypes</p>
+        <p>Score</p>
+        <p>Information</p>
+
+        <div>
+        <h4>Comments</h4>
+        <small>Summary</small>
+        </div>  
+
     </div>
   )
 }
 
-// Los campos mostrados en la ruta principal para cada receta (imagen, nombre, tipo de plato y tipo de dieta)
+// Los campos mostrados en la ruta principal para cada receta 
+// imagen
+// nombre
+// tipo de plato
+// tipo de dieta
 // Resumen del plato
 // Score
 // Paso a Paso
